4fa15c3c911e2d0acc9c179177d989dd5b613229
faiz3e <faizee.shk@gmail.com>
2020-05-31 16:56:35 +0530

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View ,TextInput } from 'react-native';
 
 export default function App() {
+  const [value, onChangeText] = React.useState('Useless Placeholder');
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Its working</Text>
+      <TextInput
+      style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+      onChangeText={text => onChangeText(text)}
+      value={value}
+    />
     </View>
   );
 }
